e5e96b8d5b0d6f131a4c6621c9b0324949a4ee6f
nadav <tashernadav@gmail.com>
2017-09-24 08:17:17 -0400

    Initial commit

---
+++ app/src/main/java/nadav/tasher/handasaim/Main.java
@@ -0,0 +1,12 @@
+package nadav.tasher.handasaim;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
