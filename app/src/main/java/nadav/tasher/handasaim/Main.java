825cc2ad7230d901de6fad2120b678af2616f03f
nadav <tashernadav@gmail.com>
2017-09-24 12:22:15 -0400

    Code 0.X

--- app/src/main/java/nadav/tasher/handasaim/Main.java
+++ app/src/main/java/nadav/tasher/handasaim/Main.java
@@ -1,73 +1,105 @@
 package nadav.tasher.handasaim;
 
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+import java.util.concurrent.ExecutionException;
 
 import nadav.tasher.lightool.Light;
 
 public class Main extends Activity {
     private final int color = Color.parseColor("#336699");
-    private final String serviceProvider="http://handasaim.co.il";
-    private final String service="http://handasaim.co.il/2017/06/13/%D7%9E%D7%A2%D7%A8%D7%9B%D7%AA-%D7%95%D7%A9%D7%99%D7%A0%D7%95%D7%99%D7%99%D7%9D/";
+    private final String serviceProvider = "http://handasaim.co.il";
+    private final String service = "http://handasaim.co.il/2017/06/13/%D7%9E%D7%A2%D7%A8%D7%9B%D7%AA-%D7%95%D7%A9%D7%99%D7%A0%D7%95%D7%99%D7%99%D7%9D/";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         startApp();
     }
+
     private void splash() {
-        final Window window=getWindow();
+        final Window window = getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(color);
         window.setNavigationBarColor(color);
-        LinearLayout ll=new LinearLayout(this);
+        LinearLayout ll = new LinearLayout(this);
         ll.setGravity(Gravity.CENTER);
         ll.setOrientation(LinearLayout.VERTICAL);
         ll.setBackgroundColor(color);
-        ImageView icon=new ImageView(this);
+        ImageView icon = new ImageView(this);
         icon.setImageDrawable(getDrawable(R.drawable.ic_icon));
-        int is=(int) (Light.Device.screenX(getApplicationContext()) * 0.8);
+        int is = (int) (Light.Device.screenX(getApplicationContext()) * 0.8);
         icon.setLayoutParams(new LinearLayout.LayoutParams(is, is));
         ll.addView(icon);
-        ProgressBar pb=new ProgressBar(this);
+        ProgressBar pb = new ProgressBar(this);
         pb.setIndeterminate(true);
         ll.addView(pb);
         setContentView(ll);
     }
-    private void checkInternet(){
-        if(Light.Device.isOnline(getApplicationContext())){
+
+    private void checkInternet() {
+        if (Light.Device.isOnline(getApplicationContext())) {
             new Light.Net.Pinger(5000, new Light.Net.Pinger.OnEnd() {
                 @Override
                 public void onPing(String s, boolean b) {
-                    if(s.equals(serviceProvider)&&b){
+                    if (s.equals(serviceProvider) && b) {
                         openApp();
-                    }else if(s.equals(serviceProvider)&&!b){
-                        popupRetry("Server Error: No Response From Service Provider.");
+                    } else if (s.equals(serviceProvider) && !b) {
+                        popup("Server Error: No Response From Service Provider.");
                     }
                 }
             }).execute(serviceProvider);
-        }else{
-            popupRetry("No Internet Connection.");
+        } else {
+            popup("No Internet Connection.");
         }
     }
-    private void startApp(){
+
+    private void startApp() {
         splash();
         checkInternet();
     }
-    void popupRetry(String text) {
-        AlertDialog.Builder pop=new AlertDialog.Builder(this);
+
+    void popup(String text) {
+        AlertDialog.Builder pop = new AlertDialog.Builder(this);
         pop.setCancelable(true);
-        pop.setMessage("Error: " + text);
-        pop.setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+        pop.setMessage(text);
+        pop.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 startApp();
@@ -81,7 +113,147 @@ public class Main extends Activity {
         });
         pop.show();
     }
-    private void openApp(){
 
+    private void openApp() {
+        getWindow().setStatusBarColor(color + 0x333333);
+        final LinearLayout all = new LinearLayout(this);
+        final LinearLayout navbarAll = new LinearLayout(this);
+        final ImageView nutIcon = new ImageView(this);
+        final int screenY = Light.Device.screenY(this);
+        final int nutSize = (screenY / 8) - screenY / 30;
+        final ObjectAnimator anim = ObjectAnimator.ofFloat(nutIcon, View.TRANSLATION_X, Light.Animations.VIBRATE_SMALL);
+        final int navY = screenY / 8;
+        final LinearLayout.LayoutParams nutParms = new LinearLayout.LayoutParams(nutSize, nutSize);
+        final LinearLayout.LayoutParams navParms = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, navY);
+        all.setOrientation(LinearLayout.VERTICAL);
+        all.setGravity(Gravity.START);
+        all.setBackgroundColor(color);
+        navbarAll.setBackgroundColor(color + 0x333333);
+        navbarAll.setOrientation(LinearLayout.HORIZONTAL);
+        navbarAll.setGravity(Gravity.CENTER);
+        nutIcon.setLayoutParams(nutParms);
+        nutIcon.setImageDrawable(getDrawable(R.drawable.ic_icon));
+        anim.setDuration(1500);
+        anim.setRepeatMode(ObjectAnimator.RESTART);
+        anim.setRepeatCount(ObjectAnimator.INFINITE);
+        anim.start();
+        navbarAll.addView(nutIcon);
+        navParms.gravity = Gravity.START;
+        navbarAll.setLayoutParams(navParms);
+        all.addView(navbarAll);
+
+        setContentView(all);
+
+        new GetLink(service, new GetLink.GotLink() {
+            @Override
+            public void onLinkGet(String link) {
+                try {
+                    String s = new getStringURL().execute(link).get();
+                    readExcelFile(s);
+                } catch (InterruptedException e) {
+                    popup("Failed, Press OK To Retry");
+                } catch (ExecutionException e) {
+                    popup("Fatal Error, Press OK To Retry");
+                }
+            }
+
+            @Override
+            public void onFail(String e) {
+                popup(e);
+            }
+        }).execute();
+    }
+
+    private static void readExcelFile(String s) {
+        try {
+
+            InputStream stream = new ByteArrayInputStream(s.getBytes(StandardCharsets.UTF_8.name()));
+            POIFSFileSystem myFileSystem = new POIFSFileSystem(stream);
+            HSSFWorkbook myWorkBook = new HSSFWorkbook(myFileSystem);
+            HSSFSheet mySheet = myWorkBook.getSheetAt(0);
+            Iterator rowIter = mySheet.rowIterator();
+            while (rowIter.hasNext()) {
+                HSSFRow myRow = (HSSFRow) rowIter.next();
+                Iterator cellIter = myRow.cellIterator();
+                while (cellIter.hasNext()) {
+                    HSSFCell myCell = (HSSFCell) cellIter.next();
+                    Log.d("CELLS", "Cell Value: " + myCell.toString());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+class GetLink extends AsyncTask<String, String, String> {
+    private String ser;
+    private GotLink gotlink;
+    private boolean success;
+
+    public GetLink(String service, GotLink gl) {
+        ser = service;
+        gotlink = gl;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        try {
+            Document docu = Jsoup.connect(ser).get();
+            Elements doc = docu.select("a");
+            String file = null;
+            for (int i = 0; i < doc.size(); i++) {
+                if (doc.get(i).attr("href").contains(".xsls") || doc.get(i).attr("href").contains(".xls")) {
+                    file = doc.get(i).attr("href");
+                    break;
+                }
+            }
+            success = true;
+            return file;
+        } catch (IOException e) {
+            success = false;
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        if (success) {
+            gotlink.onLinkGet(s);
+        } else {
+            gotlink.onFail(s);
+        }
+    }
+
+    public interface GotLink {
+        void onLinkGet(String link);
+
+        void onFail(String e);
+    }
+}
+class getStringURL extends AsyncTask<String, Integer, String> {
+
+    @Override
+    protected String doInBackground(String... params) {
+        String result = "";
+        try {
+            URL url = new URL(params[0]);
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+            in.close();
+        } catch (MalformedURLException e) {
+            return null;
+        } catch (IOException e) {
+            return null;
+        }
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
     }
 }
